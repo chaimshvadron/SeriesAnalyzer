28ac88bcc1f2f5c6f71554ae75c849e2687e797f
ChaimShvadron <s0527649958@gmail.com>
2025-05-12 16:53:58 +0300

    marge to main

--- SeriesAnalyzer/Program.cs
+++ SeriesAnalyzer/Program.cs
@@ -4,12 +4,81 @@
     {
         static void Main(string[] args)
         {
+            List<string> inputSeries()
+            {
+                Console.Write("Enter at least 3 numbers: ");
+                string inputNumbers = Console.ReadLine()!;
+
+                // validation input
+                List<string> seriesList = inputNumbers.Split("").ToList();
+                return seriesList;
+            }
+
+
             void Menu()
             {
                 // show menu
 
-                int choice = int.Parse(Console.ReadLine()!);
-
+                string choice = Console.ReadLine()!.ToLower();
+                switch (choice)
+                {
+                    case "a":
+                        {
+                            // inputSeries
+                            break;
+                        }
+                    case "b":
+                        {
+                            // displaySeries
+                            break;
+                        }
+                    case "c":
+                        {
+                            // displaySeriesRevers
+                            break;
+                        }
+                    case "d":
+                        {
+                            // displaySeriesSorted
+                            break;
+                        }
+                    case "e":
+                        {
+                            // displaySeriesMax
+                            break;
+                        }
+                    case "f":
+                        {
+                            // displaySeriesMin
+                            break;
+                        }
+                    case "g":
+                        {
+                            // displaySeriesAverage
+                            break;
+                        }
+                    case "h":
+                        {
+                            // displaySeriesNumberElements
+                            break;
+                        }
+                    case "i":
+                        {
+                            // displaySeriesAverege
+                            break;
+                        }
+                    case "j":
+                        {
+                            // exit
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("invalid choice");
+                            break;
+                            // displayMenu
+                        }
+                }
             }
         }
     }
